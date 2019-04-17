0c0dc123f35f24ffd3e6c84b1bb2f4a5cbeddf52
Junyoung Clare Jang <jjc9310@gmail.com>
2019-04-17 17:26:43 -0400

    Add a Fix data type

--- src/Minicute/Data/Fix.hs
+++ src/Minicute/Data/Fix.hs
@@ -2,12 +2,25 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE StandaloneDeriving #-}
 module Minicute.Data.Fix
-  ( Fix2(..)
+  ( Fix( .. )
+  , Fix2( .. )
   ) where
 
 import Data.Data
 import Data.Function
 import GHC.Show (appPrec, appPrec1)
+
+newtype Fix f = Fix { unFix :: f (Fix f) }
+
+deriving instance (Typeable f, Data (f (Fix f))) => Data (Fix f)
+
+instance (Eq (f (Fix f))) => Eq (Fix f) where
+  (==) = (==) `on` unFix
+
+instance (Show (f (Fix f))) => Show (Fix f) where
+  showsPrec n x
+    = showParen (n > appPrec)
+    $ ("Fix " <>) . showsPrec appPrec1 (unFix x)
 
 newtype Fix2 f a = Fix2 { unFix2 :: f (Fix2 f) a }
 
