4c0fce40c24eb2c2ceedd75ad9130d1813176be9
Junyoung Clare Jang <jjc9310@gmail.com>
2019-04-23 03:27:20 -0400

    Add basic command for testing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,44 @@
 module Main where
 
+import Control.Monad ( liftM2 )
+import Minicute.Data.PrintSequence ( toString )
+import Minicute.Parser.Parser ( programL )
+import Minicute.PrettyPrintable ( prettyPrint )
+import System.Environment
+import System.IO
+import Text.Megaparsec ( errorBundlePretty, parse )
+
 main :: IO ()
-main = print "hello!"
+main = do
+  args <- getArgs
+  case args of
+    _ : _ : _ -> usage
+    filePath : _ -> openFile filePath ReadMode >>= compile
+    [] -> compile stdin
+
+compile :: Handle -> IO ()
+compile handle = do
+  putStrLn "inputs:"
+  content <- unlines <$> whileM (hIsEOF handle) (hGetLine handle)
+  putChar '\n'
+  case parse programL "" content of
+    Right expr -> do
+      putStrLn "outputs by show:"
+      print expr
+      putStrLn "outputs by pretty printing:"
+      putStrLn (toString (prettyPrint expr))
+    Left err -> do
+      putStrLn "error:"
+      putStrLn (errorBundlePretty err)
+
+whileM :: (Monad m) => m Bool -> m a -> m [a]
+whileM cond act = go
+  where
+    go = do
+      b <- cond
+      if b
+      then return []
+      else liftM2 (:) act go
+
+usage :: IO ()
+usage = putStrLn "Usage: minicute [file]"
