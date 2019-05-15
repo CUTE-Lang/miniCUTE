0580ea1df99563ad08ab06b16fddb24b8956cde1
Junyoung Clare Jang <jjc9310@gmail.com>
2019-05-15 19:54:54 -0400

    Add tests for lambda lifting

--- test/Minicute/Transpiler/LambdaLiftingSpec.hs
+++ test/Minicute/Transpiler/LambdaLiftingSpec.hs
@@ -25,12 +25,83 @@ type TestBeforeContent = MainProgramL
 type TestAfterContent = MainProgram
 type TestCase = (TestName, TestBeforeContent, TestAfterContent)
 
+-- |
+-- TODO - Introduce quosiquoter for 'Program' and update
+-- these test cases
 testCases :: [TestCase]
 testCases =
   [ ( "empty program"
-    , Program
+    , ProgramL
       []
     , Program
       []
+    )
+
+  , ( "program with single lambda as a body of a top-level definition"
+    , ProgramL
+      [ ( "f"
+        , []
+        , ELLambda ["x"] (ELVariable "x")
+        )
+      ]
+    , Program
+      [ ( "f0"
+        , []
+        , EVariable "annon1"
+        )
+      , ( "annon1"
+        , ["x2"]
+        , EVariable "x2"
+        )
+      ]
+    )
+
+  , ( "program with let expression containing lambdas"
+    , ProgramL
+      [ ( "f"
+        , []
+        , ELLet
+          NonRecursive
+          [ ( "g"
+            , ELLambda ["x"] (ELVariable "x")
+            )
+          , ( "h"
+            , ELLambda ["x"] (ELApplication2 (ELVariable "*") (ELVariable "x") (ELVariable "x"))
+            )
+          ]
+          ( ELApplication2
+            (ELVariable "+")
+            (ELApplication (ELVariable "g") (ELInteger 5))
+            (ELApplication (ELVariable "h") (ELInteger 4))
+          )
+        )
+      ]
+    , Program
+      [ ( "f0"
+        , []
+        , ELet
+          NonRecursive
+          [ ( "g1"
+            , EVariable "annon3"
+            )
+          , ( "h2"
+            , EVariable "annon5"
+            )
+          ]
+          ( EApplication2
+            (EVariable "+")
+            (EApplication (EVariable "g1") (EInteger 5))
+            (EApplication (EVariable "h2") (EInteger 4))
+          )
+        )
+      , ( "annon3"
+        , ["x4"]
+        , EVariable "x4"
+        )
+      , ( "annon5"
+        , ["x6"]
+        , EApplication2 (EVariable "*") (EVariable "x6") (EVariable "x6")
+        )
+      ]
     )
   ]
