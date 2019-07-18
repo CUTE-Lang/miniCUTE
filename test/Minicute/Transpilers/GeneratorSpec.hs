5c3fdc67ce3d00fb900911f2fa931ce67d3abff8
Junyoung Clare Jang <jjc9310@gmail.com>
2019-07-18 14:52:32 -0400

    Update test case for generator

--- test/Minicute/Transpilers/GeneratorSpec.hs
+++ test/Minicute/Transpilers/GeneratorSpec.hs
@@ -9,14 +9,15 @@ import Test.Hspec
 
 import Control.Monad
 import Data.Tuple.Extra
+import Data.Word
 import LLVM.AST.Global ( name, basicBlocks )
 import LLVM.AST.Instruction ( Named( (:=) ) )
 import Minicute.Transpilers.Generator
 import Minicute.Utils.TH
 
 import qualified LLVM.AST as AST
-import qualified LLVM.AST.Constant as AST
-import qualified LLVM.AST.Type as AST
+import qualified LLVM.AST.Constant as ASTC
+import qualified LLVM.AST.Type as ASTT
 
 spec :: Spec
 spec = do
@@ -52,18 +53,41 @@ testCases
            }
         |]
       , [ AST.GlobalDefinition AST.functionDefaults
-          { name = "__user_defined__f"
+          { name = "minicute__user__defined__f"
           , basicBlocks =
               [ AST.BasicBlock
-                (AST.UnName 0)
-                [ AST.UnName 1 := AST.Alloca AST.i32 Nothing 1 []
+                "entry"
+                [ AST.UnName 0 := AST.Alloca ASTT.i32 Nothing 0 []
                 , AST.Do
                   ( AST.Store
                     False
-                    (AST.ConstantOperand (AST.Int 32 100))
-                    (AST.LocalReference (AST.ptr AST.i32) (AST.UnName 1))
+                    (operandInt 32 100)
+                    (AST.LocalReference typeInt32Ptr (AST.UnName 0))
                     Nothing
-                    1
+                    0
+                    []
+                  )
+                , AST.UnName 1 := AST.Load False (AST.LocalReference typeInt32Ptr (AST.UnName 0)) Nothing 0 []
+
+                , AST.UnName 2 := AST.BitCast (AST.ConstantOperand (ASTC.GetElementPtr True constantAddrStackPointer [ASTC.Int 32 0])) typeNodeNInteger []
+                , AST.UnName 3 := AST.GetElementPtr True (AST.LocalReference typeNodeNIntegerPtr (AST.UnName 2)) [operandInt 32 0, operandInt 32 0] []
+                , AST.Do
+                  ( AST.Store
+                    False
+                    (operandInt 32 1)
+                    (AST.LocalReference typeInt32Ptr (AST.UnName 3))
+                    Nothing
+                    0
+                    []
+                  )
+                , AST.UnName 4 := AST.GetElementPtr True (AST.LocalReference typeNodeNIntegerPtr (AST.UnName 2)) [operandInt 32 0, operandInt 32 1] []
+                , AST.Do
+                  ( AST.Store
+                    False
+                    (AST.LocalReference typeInt32 (AST.UnName 1))
+                    (AST.LocalReference typeInt32Ptr (AST.UnName 4))
+                    Nothing
+                    0
                     []
                   )
                 ]
@@ -73,3 +97,24 @@ testCases
         ]
       )
     ]
+
+operandInt :: Word32 -> Integer -> AST.Operand
+operandInt w n = AST.ConstantOperand (ASTC.Int w n)
+
+constantAddrStackPointer :: ASTC.Constant
+constantAddrStackPointer = ASTC.GlobalReference typeInt32PtrPtr "asp"
+
+typeNodeNIntegerPtr :: ASTT.Type
+typeNodeNIntegerPtr = ASTT.ptr typeNodeNInteger
+
+typeNodeNInteger :: ASTT.Type
+typeNodeNInteger = ASTT.NamedTypeReference "node.NInteger"
+
+typeInt32PtrPtr :: ASTT.Type
+typeInt32PtrPtr = ASTT.ptr typeInt32Ptr
+
+typeInt32Ptr :: ASTT.Type
+typeInt32Ptr = ASTT.ptr typeInt32
+
+typeInt32 :: ASTT.Type
+typeInt32 = ASTT.i32
