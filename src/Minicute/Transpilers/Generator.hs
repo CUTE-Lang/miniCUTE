df5aeec71a7bd82de665369c41ab5aebfb283e7a
Junyoung Clare Jang <jjc9310@gmail.com>
2019-07-18 20:58:06 -0400

    Fix a wrong spec and generator

--- src/Minicute/Transpilers/Generator.hs
+++ src/Minicute/Transpilers/Generator.hs
@@ -37,13 +37,23 @@ generateMachineCodeE = go []
       pName <- emitInstr typeInt32Ptr (AST.Alloca ASTT.i32 Nothing 0 [])
       emitInstrVoid (AST.Store False (operandInt 32 v) pName Nothing 0 [])
       vName <- emitInstr typeInt32 (AST.Load False pName Nothing 0 [])
+
       go (vName : vStack) insts
     go (vName : vStack) (IUpdateAsInteger n : insts@(_ : _)) = do
-      nName <- emitInstr typeNodeNIntegerPtr (AST.BitCast (AST.ConstantOperand (ASTC.GetElementPtr True constantAddrStackPointer [ASTC.Int 32 (negate (toInteger n))])) typeNodeNInteger [])
+      hName <- emitInstr typeInt8Ptr (AST.Load False (AST.ConstantOperand constantNodeHeapPointer) Nothing 0 [])
+      nName <- emitInstr typeNodeNIntegerPtr (AST.BitCast hName typeNodeNIntegerPtr [])
+      nName' <- emitInstr typeNodeNIntegerPtr (AST.GetElementPtr True nName [operandInt 32 1] [])
+      hName' <- emitInstr typeInt8Ptr (AST.BitCast nName' typeInt8Ptr [])
+      emitInstrVoid (AST.Store False hName' (AST.ConstantOperand constantNodeHeapPointer) Nothing 0 [])
       tName <- emitInstr typeInt32Ptr (AST.GetElementPtr True nName [operandInt 32 0, operandInt 32 0] [])
       emitInstrVoid (AST.Store False (operandInt 32 1) tName Nothing 0 [])
       fName <- emitInstr typeInt32Ptr (AST.GetElementPtr True nName [operandInt 32 0, operandInt 32 1] [])
       emitInstrVoid (AST.Store False vName fName Nothing 0 [])
+
+      sName <- emitInstr typeInt8PtrPtr (AST.Load False (AST.ConstantOperand constantAddrStackPointer) Nothing 0 [])
+      sName' <- emitInstr typeInt8PtrPtr (AST.GetElementPtr True sName [operandInt 32 (toInteger (negate n))] [])
+      emitInstrVoid (AST.Store False hName sName' Nothing 0 [])
+
       go vStack insts
     go _ _ = emitTerm (AST.Ret Nothing [])
 
@@ -54,13 +64,31 @@ operandInt :: Word32 -> Integer -> AST.Operand
 operandInt w n = AST.ConstantOperand (ASTC.Int w n)
 
 constantAddrStackPointer :: ASTC.Constant
-constantAddrStackPointer = ASTC.GlobalReference typeInt32PtrPtr "asp"
+constantAddrStackPointer = ASTC.GlobalReference typeInt8PtrPtrPtr "asp"
+
+constantNodeHeapPointer :: ASTC.Constant
+constantNodeHeapPointer = ASTC.GlobalReference typeInt8PtrPtr "nhp"
 
 typeNodeNIntegerPtr :: ASTT.Type
 typeNodeNIntegerPtr = ASTT.ptr typeNodeNInteger
 
 typeNodeNInteger :: ASTT.Type
 typeNodeNInteger = ASTT.NamedTypeReference "node.NInteger"
+
+typeInt8PtrPtrPtr :: ASTT.Type
+typeInt8PtrPtrPtr = ASTT.ptr typeInt8PtrPtr
+
+typeInt8PtrPtr :: ASTT.Type
+typeInt8PtrPtr = ASTT.ptr typeInt8Ptr
+
+typeInt8Ptr :: ASTT.Type
+typeInt8Ptr = ASTT.ptr typeInt8
+
+typeInt8 :: ASTT.Type
+typeInt8 = ASTT.i8
+
+typeInt32PtrPtrPtr :: ASTT.Type
+typeInt32PtrPtrPtr = ASTT.ptr typeInt32PtrPtr
 
 typeInt32PtrPtr :: ASTT.Type
 typeInt32PtrPtr = ASTT.ptr typeInt32Ptr
--- test/Minicute/Transpilers/GeneratorSpec.hs
+++ test/Minicute/Transpilers/GeneratorSpec.hs
@@ -10,7 +10,6 @@ import Test.Hspec
 import Control.Monad
 import Data.Tuple.Extra
 import Data.Word
-import LLVM.AST.Global ( name, returnType, basicBlocks )
 import LLVM.IRBuilder
 import Minicute.Transpilers.Generator
 import Minicute.Utils.TH
@@ -64,11 +63,19 @@ testCases
               vName <- emitInstr typeInt32 (AST.Load False (AST.LocalReference typeInt32Ptr (AST.UnName 0)) Nothing 0 [])
 
               -- UpdateAsInteger 0
-              nName <- emitInstr typeNodeNIntegerPtr (AST.BitCast (AST.ConstantOperand (ASTC.GetElementPtr True constantAddrStackPointer [ASTC.Int 32 0])) typeNodeNInteger [])
+              hName <- emitInstr typeInt8Ptr (AST.Load False (AST.ConstantOperand constantNodeHeapPointer) Nothing 0 [])
+              nName <- emitInstr typeNodeNIntegerPtr (AST.BitCast hName typeNodeNIntegerPtr [])
+              nName' <- emitInstr typeNodeNIntegerPtr (AST.GetElementPtr True nName [operandInt 32 1] [])
+              hName' <- emitInstr typeInt8Ptr (AST.BitCast nName' typeInt8Ptr [])
+              emitInstrVoid (AST.Store False hName' (AST.ConstantOperand constantNodeHeapPointer) Nothing 0 [])
               tName <- emitInstr typeInt32Ptr (AST.GetElementPtr True nName [operandInt 32 0, operandInt 32 0] [])
               emitInstrVoid (AST.Store False (operandInt 32 1) tName Nothing 0 [])
               fName <- emitInstr typeInt32Ptr (AST.GetElementPtr True nName [operandInt 32 0, operandInt 32 1] [])
               emitInstrVoid (AST.Store False vName fName Nothing 0 [])
+
+              sName <- emitInstr typeInt8PtrPtr (AST.Load False (AST.ConstantOperand constantAddrStackPointer) Nothing 0 [])
+              sName' <- emitInstr typeInt8PtrPtr (AST.GetElementPtr True sName [operandInt 32 0] [])
+              emitInstrVoid (AST.Store False hName sName' Nothing 0 [])
 
               -- Return
               -- __TODO: this is not correct. Exchange @asb@ with @asp@__
@@ -81,13 +88,31 @@ operandInt :: Word32 -> Integer -> AST.Operand
 operandInt w n = AST.ConstantOperand (ASTC.Int w n)
 
 constantAddrStackPointer :: ASTC.Constant
-constantAddrStackPointer = ASTC.GlobalReference typeInt32PtrPtr "asp"
+constantAddrStackPointer = ASTC.GlobalReference typeInt8PtrPtrPtr "asp"
+
+constantNodeHeapPointer :: ASTC.Constant
+constantNodeHeapPointer = ASTC.GlobalReference typeInt8PtrPtr "nhp"
 
 typeNodeNIntegerPtr :: ASTT.Type
 typeNodeNIntegerPtr = ASTT.ptr typeNodeNInteger
 
 typeNodeNInteger :: ASTT.Type
 typeNodeNInteger = ASTT.NamedTypeReference "node.NInteger"
+
+typeInt8PtrPtrPtr :: ASTT.Type
+typeInt8PtrPtrPtr = ASTT.ptr typeInt8PtrPtr
+
+typeInt8PtrPtr :: ASTT.Type
+typeInt8PtrPtr = ASTT.ptr typeInt8Ptr
+
+typeInt8Ptr :: ASTT.Type
+typeInt8Ptr = ASTT.ptr typeInt8
+
+typeInt8 :: ASTT.Type
+typeInt8 = ASTT.i8
+
+typeInt32PtrPtrPtr :: ASTT.Type
+typeInt32PtrPtrPtr = ASTT.ptr typeInt32PtrPtr
 
 typeInt32PtrPtr :: ASTT.Type
 typeInt32PtrPtr = ASTT.ptr typeInt32Ptr
