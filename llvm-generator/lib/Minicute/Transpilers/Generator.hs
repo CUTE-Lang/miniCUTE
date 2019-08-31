40cce7a26c65a37f97d46602dcf53b917017564e
Ailrun <jjc9310@gmail.com>
2019-08-31 15:20:20 -0400

    Extract common LLVM constants into a new module

--- llvm-generator/lib/Minicute/Transpilers/Generator.hs
+++ llvm-generator/lib/Minicute/Transpilers/Generator.hs
@@ -6,13 +6,12 @@ module Minicute.Transpilers.Generator
 
 import Control.Monad
 import Data.String
-import Data.Word
 import LLVM.IRBuilder
 import Minicute.Data.GMachine.Instruction
 import Minicute.Data.Minicute.Common
+import Minicute.Transpilers.Constants
 
 import qualified LLVM.AST as AST
-import qualified LLVM.AST.Constant as ASTC
 import qualified LLVM.AST.Type as ASTT
 
 generateMachineCode :: GMachineProgram -> [AST.Definition]
@@ -52,54 +51,3 @@ generateMachineCodeE = go []
       store bName' 0 (AST.ConstantOperand constantAddrStackPointer)
       retVoid
     go _ _ = error "generateMachineCodeE: Not yet implemented"
-
--- *
--- We should extract or export the following definitions
--- since tests use them too.
-operandInt :: Word32 -> Integer -> AST.Operand
-operandInt w n = AST.ConstantOperand (ASTC.Int w n)
-
-constantUpdateNodeNInteger :: ASTC.Constant
-constantUpdateNodeNInteger = ASTC.GlobalReference typeUpdateNodeNInteger "minicute_update_node_NInteger"
-
-constantAddrStackPointer :: ASTC.Constant
-constantAddrStackPointer = ASTC.GlobalReference typeInt8PtrPtrPtr "asp"
-
-constantAddrBasePointer :: ASTC.Constant
-constantAddrBasePointer = ASTC.GlobalReference typeInt8PtrPtrPtr "abp"
-
-constantNodeHeapPointer :: ASTC.Constant
-constantNodeHeapPointer = ASTC.GlobalReference typeInt8PtrPtr "nhp"
-
-typeUpdateNodeNInteger :: ASTT.Type
-typeUpdateNodeNInteger = ASTT.FunctionType ASTT.void [typeInt32, typeInt8Ptr] False
-
-typeNodeNIntegerPtr :: ASTT.Type
-typeNodeNIntegerPtr = ASTT.ptr typeNodeNInteger
-
-typeNodeNInteger :: ASTT.Type
-typeNodeNInteger = ASTT.NamedTypeReference "node.NInteger"
-
-typeInt8PtrPtrPtr :: ASTT.Type
-typeInt8PtrPtrPtr = ASTT.ptr typeInt8PtrPtr
-
-typeInt8PtrPtr :: ASTT.Type
-typeInt8PtrPtr = ASTT.ptr typeInt8Ptr
-
-typeInt8Ptr :: ASTT.Type
-typeInt8Ptr = ASTT.ptr typeInt8
-
-typeInt8 :: ASTT.Type
-typeInt8 = ASTT.i8
-
-typeInt32PtrPtrPtr :: ASTT.Type
-typeInt32PtrPtrPtr = ASTT.ptr typeInt32PtrPtr
-
-typeInt32PtrPtr :: ASTT.Type
-typeInt32PtrPtr = ASTT.ptr typeInt32Ptr
-
-typeInt32Ptr :: ASTT.Type
-typeInt32Ptr = ASTT.ptr typeInt32
-
-typeInt32 :: ASTT.Type
-typeInt32 = ASTT.i32
--- llvm-generator/test/Minicute/Transpilers/GeneratorSpec.hs
+++ llvm-generator/test/Minicute/Transpilers/GeneratorSpec.hs
@@ -9,13 +9,12 @@ import Test.Hspec
 
 import Control.Monad
 import Data.Tuple.Extra
-import Data.Word
 import LLVM.IRBuilder
+import Minicute.Transpilers.Constants
 import Minicute.Transpilers.Generator
 import Minicute.Utils.TH
 
 import qualified LLVM.AST as AST
-import qualified LLVM.AST.Constant as ASTC
 import qualified LLVM.AST.Type as ASTT
 
 spec :: Spec
@@ -76,51 +75,3 @@ testCases
         )
       )
     ]
-
-operandInt :: Word32 -> Integer -> AST.Operand
-operandInt w n = AST.ConstantOperand (ASTC.Int w n)
-
-constantUpdateNodeNInteger :: ASTC.Constant
-constantUpdateNodeNInteger = ASTC.GlobalReference typeUpdateNodeNInteger "minicute_update_node_NInteger"
-
-constantAddrStackPointer :: ASTC.Constant
-constantAddrStackPointer = ASTC.GlobalReference typeInt8PtrPtrPtr "asp"
-
-constantAddrBasePointer :: ASTC.Constant
-constantAddrBasePointer = ASTC.GlobalReference typeInt8PtrPtrPtr "abp"
-
-constantNodeHeapPointer :: ASTC.Constant
-constantNodeHeapPointer = ASTC.GlobalReference typeInt8PtrPtr "nhp"
-
-typeUpdateNodeNInteger :: ASTT.Type
-typeUpdateNodeNInteger = ASTT.FunctionType ASTT.void [typeInt32, typeInt8Ptr] False
-
-typeNodeNIntegerPtr :: ASTT.Type
-typeNodeNIntegerPtr = ASTT.ptr typeNodeNInteger
-
-typeNodeNInteger :: ASTT.Type
-typeNodeNInteger = ASTT.NamedTypeReference "node.NInteger"
-
-typeInt8PtrPtrPtr :: ASTT.Type
-typeInt8PtrPtrPtr = ASTT.ptr typeInt8PtrPtr
-
-typeInt8PtrPtr :: ASTT.Type
-typeInt8PtrPtr = ASTT.ptr typeInt8Ptr
-
-typeInt8Ptr :: ASTT.Type
-typeInt8Ptr = ASTT.ptr typeInt8
-
-typeInt8 :: ASTT.Type
-typeInt8 = ASTT.i8
-
-typeInt32PtrPtrPtr :: ASTT.Type
-typeInt32PtrPtrPtr = ASTT.ptr typeInt32PtrPtr
-
-typeInt32PtrPtr :: ASTT.Type
-typeInt32PtrPtr = ASTT.ptr typeInt32Ptr
-
-typeInt32Ptr :: ASTT.Type
-typeInt32Ptr = ASTT.ptr typeInt32
-
-typeInt32 :: ASTT.Type
-typeInt32 = ASTT.i32
