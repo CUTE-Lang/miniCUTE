bb9dbb137aea77bc5d67ef4ef98608e5cdee26e0
Junyoung Clare Jang <jjc9310@gmail.com>
2019-09-05 19:23:14 -0400

    Add extra runtime types for haskell constants

--- llvm-generator/lib/Minicute/Transpilers/Constants.hs
+++ llvm-generator/lib/Minicute/Transpilers/Constants.hs
@@ -28,11 +28,23 @@ operandNGlobal (Identifier iStr)
 operandCreateNodeNInteger :: AST.Operand
 operandCreateNodeNInteger = AST.ConstantOperand constantCreateNodeNInteger
 
+operandCreateNodeNStructure :: AST.Operand
+operandCreateNodeNStructure = AST.ConstantOperand constantCreateNodeNStructure
+
+operandCreateNodeNStructureFields :: AST.Operand
+operandCreateNodeNStructureFields = AST.ConstantOperand constantCreateNodeNStructureFields
+
 operandCreateNodeNApplication :: AST.Operand
 operandCreateNodeNApplication = AST.ConstantOperand constantCreateNodeNApplication
 
 operandUpdateNodeNInteger :: AST.Operand
 operandUpdateNodeNInteger = AST.ConstantOperand constantUpdateNodeNInteger
+
+operandUpdateNodeNStructure :: AST.Operand
+operandUpdateNodeNStructure = AST.ConstantOperand constantUpdateNodeNStructure
+
+operandUpdateNodeNApplication :: AST.Operand
+operandUpdateNodeNApplication = AST.ConstantOperand constantUpdateNodeNApplication
 
 operandUpdateNodeNIndirect :: AST.Operand
 operandUpdateNodeNIndirect = AST.ConstantOperand constantUpdateNodeNIndirect
@@ -52,11 +64,23 @@ operandNodeHeapPointer = AST.ConstantOperand constantNodeHeapPointer
 constantCreateNodeNInteger :: ASTC.Constant
 constantCreateNodeNInteger = ASTC.GlobalReference typeCreateNodeNInteger "minicute_create_node_NInteger"
 
+constantCreateNodeNStructure :: ASTC.Constant
+constantCreateNodeNStructure = ASTC.GlobalReference typeCreateNodeNStructure "minicute_create_node_NStructure"
+
+constantCreateNodeNStructureFields :: ASTC.Constant
+constantCreateNodeNStructureFields = ASTC.GlobalReference typeCreateNodeNStructureFields "minicute_create_node_NStructureFields"
+
 constantCreateNodeNApplication :: ASTC.Constant
 constantCreateNodeNApplication = ASTC.GlobalReference typeCreateNodeNApplication "minicute_create_node_NApplication"
 
 constantUpdateNodeNInteger :: ASTC.Constant
 constantUpdateNodeNInteger = ASTC.GlobalReference typeUpdateNodeNInteger "minicute_update_node_NInteger"
+
+constantUpdateNodeNStructure :: ASTC.Constant
+constantUpdateNodeNStructure = ASTC.GlobalReference typeUpdateNodeNStructure "minicute_update_node_NStructure"
+
+constantUpdateNodeNApplication :: ASTC.Constant
+constantUpdateNodeNApplication = ASTC.GlobalReference typeUpdateNodeNApplication "minicute_update_node_NApplication"
 
 constantUpdateNodeNIndirect :: ASTC.Constant
 constantUpdateNodeNIndirect = ASTC.GlobalReference typeUpdateNodeNIndirect "minicute_update_node_NIndirect"
@@ -76,11 +100,23 @@ constantNodeHeapPointer = ASTC.GlobalReference typeInt8PtrPtr "nhp"
 typeCreateNodeNInteger :: ASTT.Type
 typeCreateNodeNInteger = ASTT.FunctionType typeInt8Ptr [typeInt32] False
 
+typeCreateNodeNStructure :: ASTT.Type
+typeCreateNodeNStructure = ASTT.FunctionType typeInt8Ptr [typeInt32, typeInt8Ptr] False
+
+typeCreateNodeNStructureFields :: ASTT.Type
+typeCreateNodeNStructureFields = ASTT.FunctionType typeInt8Ptr [typeInt32, typeInt8Ptr] False
+
 typeCreateNodeNApplication :: ASTT.Type
 typeCreateNodeNApplication = ASTT.FunctionType typeInt8Ptr [typeInt8Ptr, typeInt8Ptr] False
 
 typeUpdateNodeNInteger :: ASTT.Type
 typeUpdateNodeNInteger = ASTT.FunctionType ASTT.void [typeInt32, typeInt8Ptr] False
+
+typeUpdateNodeNStructure :: ASTT.Type
+typeUpdateNodeNStructure = ASTT.FunctionType ASTT.void [typeInt32, typeInt8Ptr, typeInt8Ptr] False
+
+typeUpdateNodeNApplication :: ASTT.Type
+typeUpdateNodeNApplication = ASTT.FunctionType ASTT.void [typeInt8Ptr, typeInt8Ptr, typeInt8Ptr] False
 
 typeUpdateNodeNIndirect :: ASTT.Type
 typeUpdateNodeNIndirect = ASTT.FunctionType ASTT.void [typeInt8Ptr, typeInt8Ptr] False
