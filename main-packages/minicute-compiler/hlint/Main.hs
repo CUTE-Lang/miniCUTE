37559f40ad8291adcd8c092808fec0a8dd47df53
Junyoung Clare Jang <jjc9310@gmail.com>
2019-09-29 20:56:18 -0400

    Move main packages into main-packages directory

---
+++ main-packages/minicute-compiler/hlint/Main.hs
@@ -0,0 +1,21 @@
+module Main
+  ( main
+  ) where
+
+import Language.Haskell.HLint3 ( hlint )
+import System.Exit ( exitFailure, exitSuccess )
+
+main :: IO ()
+main =
+  do
+    -- This 'putStrLn' is to format stack test output
+    putStrLn ""
+    let
+      hlintArgs =
+        [ "minicute"
+        , "--hint=../../hlint.yaml"
+        ]
+    hints <- hlint hlintArgs
+    if null hints
+    then exitSuccess
+    else exitFailure
