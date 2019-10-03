9242b923edeb2b14a2ad5044b27dd127beffae19
Junyoung Clare Jang <jjc9310@gmail.com>
2019-10-03 00:01:14 -0400

    Add a primitive main

--- main-packages/g-machine-interpreter/minicute-g/Main.hs
+++ main-packages/g-machine-interpreter/minicute-g/Main.hs
@@ -2,5 +2,29 @@ module Main
   ( main
   ) where
 
+import Minicute.Parser.GMachine.Parser ( gMachineProgram )
+import System.Environment
+import System.IO
+import Text.Megaparsec
+
 main :: IO ()
-main = putStrLn "hi"
+main = do
+  args <- getArgs
+  case args of
+    _ : _ : _ -> usage
+    filePath : _ -> openFile filePath ReadMode >>= interpret
+    [] -> interpret stdin
+
+interpret :: Handle -> IO ()
+interpret handle = do
+  content <- hGetContents handle
+  case parse gMachineProgram "" content of
+    Right program -> do
+      putStrLn "program by show:"
+      print program
+    Left err -> do
+      putStrLn "error:"
+      putStrLn (errorBundlePretty err)
+
+usage :: IO ()
+usage = putStrLn "Usage: minicute-g [file]"
