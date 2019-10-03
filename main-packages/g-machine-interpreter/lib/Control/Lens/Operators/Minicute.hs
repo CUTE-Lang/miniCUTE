80ddaf289ac0f5f28edeb39f34303bf1b7d51fae
Junyoung Clare Jang <jjc9310@gmail.com>
2019-10-03 16:12:16 -0400

    Introduce custom lens operators

---
+++ main-packages/g-machine-interpreter/lib/Control/Lens/Operators/Minicute.hs
@@ -0,0 +1,17 @@
+module Control.Lens.Operators.Minicute
+  ( (%%~=)
+  ) where
+
+import Control.Lens.Internal.Getter ( AlongsideLeft(..), getAlongsideLeft )
+import Control.Lens.Type
+import Control.Monad.State ( MonadState, gets, put )
+import Data.Tuple ( swap )
+
+infixr 4 %%~=
+
+(%%~=) :: (MonadState s m) => LensLike (AlongsideLeft m r) s s a b -> (a -> m (r, b)) -> m r
+_l %%~= f = do
+  along <- gets $ _l (AlongsideLeft . fmap swap . f)
+  (st, res) <- getAlongsideLeft along
+  put st
+  pure res
