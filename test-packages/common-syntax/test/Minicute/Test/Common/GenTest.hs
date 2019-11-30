f1235af802458f25188190500c58ae953e78712c
Junyoung Clare Jang <jjc9310@gmail.com>
2019-11-30 18:27:53 -0500

    Add a package for tests using minicute-common-syntax

---
+++ test-packages/common-syntax/test/Minicute/Test/Common/GenTest.hs
@@ -0,0 +1,36 @@
+{- HLINT ignore "Redundant do" -}
+{- HLINT ignore "Use camelCase" -}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+{-# LANGUAGE BinaryLiterals #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+-- |
+-- Copyright: (c) 2018-present Junyoung Clare Jang
+-- License: BSD 3-Clause
+module Minicute.Test.Common.GenTest
+  ( hprop_integer_gen_is_compatible_with_integer_lexer
+
+  , hprop_identifier_gen_is_compatible_with_identifier_lexer
+  ) where
+
+import Hedgehog
+
+import Minicute.Parser.Common ( Parser )
+import Text.Megaparsec hiding ( failure )
+
+import qualified Minicute.Parser.Common.Lexer as L
+import qualified Minicute.Test.Common.Gen as MCGen
+
+hprop_integer_gen_is_compatible_with_integer_lexer :: Property
+hprop_integer_gen_is_compatible_with_integer_lexer = do
+  property $ do
+    i <- forAll MCGen.integer
+    i' <- evalEither $ parse (L.integer :: Parser Integer) "" (show i)
+    i === i'
+
+hprop_identifier_gen_is_compatible_with_identifier_lexer :: Property
+hprop_identifier_gen_is_compatible_with_identifier_lexer = do
+  property $ do
+    ident <- forAll MCGen.identifier
+    ident' <- evalEither $ parse (L.identifier :: Parser String) "" ident
+    ident === ident'
