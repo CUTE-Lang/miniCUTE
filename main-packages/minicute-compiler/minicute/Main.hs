2643544169564565ff01e85e197b842cb40071de
Junyoung Clare Jang <jjc9310@gmail.com>
2019-12-01 09:20:01 -0500

    Update minicute to accept some options

--- main-packages/minicute-compiler/minicute/Main.hs
+++ main-packages/minicute-compiler/minicute/Main.hs
@@ -7,20 +7,25 @@ module Main
   ( main
   ) where
 
+import Control.Lens.Lens ( Lens, lens )
+import Control.Lens.Operators
+import Control.Monad ( foldM )
+import Control.Monad.Identity ( Identity(..) )
 import Data.Text.Prettyprint.Doc.Minicute ( prettyMC0 )
 import Minicute.Parser.Minicute.Parser ( mainProgramMC )
 import Minicute.Transpilers.Lifting.Lambda ( lambdaLifting )
 import System.Environment
+import System.Exit
 import System.IO
 import Text.Megaparsec ( errorBundlePretty, parse )
+
+import qualified Data.List.NonEmpty as NonEmpty
 
 main :: IO ()
 main = do
   args <- getArgs
-  case args of
-    _ : _ : _ -> usage
-    filePath : _ -> openFile filePath ReadMode >>= compile
-    [] -> compile stdin
+  options <- parseOptions args
+  compile . runIdentity $ input options
 
 compile :: Handle -> IO ()
 compile handle = do
@@ -42,5 +47,45 @@ compile handle = do
       putStrLn "error:"
       putStrLn (errorBundlePretty err)
 
+-- |
+-- _TODO: move this type and related functions to a library_
+newtype Options ih
+  = Options
+    { input :: ih Handle
+    }
+
+_input :: Lens (Options ih) (Options ih') (ih Handle) (ih' Handle)
+_input = lens input setter
+  where
+    setter opts input = Options{ input = input }
+
+type PartialOptions = Options Maybe
+type ValidOptions = Options Identity
+
+parseOptions :: [String] -> IO ValidOptions
+parseOptions = fmap elaborate . go initialOptions
+  where
+    elaborate = _input %~ elaboratedInputHandles
+      where
+        elaboratedInputHandles Nothing = Identity stdin
+        elaboratedInputHandles (Just h) = Identity h
+
+    go = foldM parseOption
+
+parseOption :: PartialOptions -> String -> IO PartialOptions
+parseOption opts "-h" = usage >> exitSuccess
+parseOption opts "--help" = usage >> exitSuccess
+parseOption Options{ input = Just _ } filePath = usage >> exitFailure
+parseOption opts filePath
+  = opts & _input %%~ (\_ -> Just <$> openFile filePath ReadMode)
+
+initialOptions :: PartialOptions
+initialOptions
+  = Options
+    { input = Nothing
+    }
+
 usage :: IO ()
-usage = putStrLn "Usage: minicute [file]"
+usage = do
+  progName <- getProgName
+  putStrLn $ "Usage: " <> progName <> " [file]"
