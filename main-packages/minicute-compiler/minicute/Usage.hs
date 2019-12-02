09592d83ad94ffe554a9ba163d3c6e16d1ed54e6
Junyoung Clare Jang <jjc9310@gmail.com>
2019-12-02 01:53:25 -0500

    Update usage output for minicute executable

--- main-packages/minicute-compiler/minicute/Usage.hs
+++ main-packages/minicute-compiler/minicute/Usage.hs
@@ -2,9 +2,17 @@ module Usage
   ( usage
   ) where
 
+import Data.Foldable
 import System.Environment
 
 usage :: IO ()
 usage = do
   progName <- getProgName
-  putStrLn $ "Usage: " <> progName <> " [file]"
+  traverse_ putStrLn
+    [ "Usage: " <> progName <> " [OPTIONS]... [FILE]"
+    , "Execute miniCUTE compile steps against FILE."
+    , "With no FILE, read standard input."
+    , ""
+    , "Options:"
+    , "  --help, -h        display this message and exit successfully"
+    ]
