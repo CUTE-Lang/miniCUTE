da497bec7d0c8d73e0c4c86a7a7fb00ef7546365
Junyoung Clare Jang <jjc9310@gmail.com>
2019-12-16 01:38:02 -0500

    Refactor prettyprinter-minicute package

---
+++ extensions/prettyprinter/lib/Data/Text/Prettyprint/Doc/Minicute/Internal/TH.hs
@@ -0,0 +1,24 @@
+module Data.Text.Prettyprint.Doc.Minicute.Internal.TH
+  ( makePrettyMCFromPretty
+  ) where
+
+import Language.Haskell.TH.Lib
+import Language.Haskell.TH.Syntax
+
+makePrettyMCFromPretty :: Name -> DecsQ
+makePrettyMCFromPretty tName = do
+  let
+    prettyMCFunctionName = mkName "prettyMC"
+    prettyListMCFunctionName = mkName "prettyListMC"
+    prettyMCClassName = mkName "Data.Text.Prettyprint.Doc.Minicute.PrettyMC"
+    prettyMCInstance
+      = instanceD (cxt []) (conT prettyMCClassName `appT` conT tName)
+        [ funD prettyMCFunctionName
+          [clause [wildP] (normalB (varE (mkName "Data.Text.Prettyprint.Doc.pretty"))) []]
+        , funD prettyListMCFunctionName
+          [clause [wildP] (normalB (varE (mkName "Data.Text.Prettyprint.Doc.prettyList"))) []]
+        , pragInlD prettyMCFunctionName Inlinable FunLike AllPhases
+        , pragInlD prettyListMCFunctionName Inlinable FunLike AllPhases
+        ]
+  pure <$>  prettyMCInstance
+
