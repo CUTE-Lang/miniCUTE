83adc877ff4829896c93a9506fe2798895a8da31
Junyoung Clare Jang <jjc9310@gmail.com>
2019-12-17 02:51:01 -0500

    Refactoring hlint-minicute-utility package

--- utilities/hlint/hlint-run/Main.hs
+++ utilities/hlint/hlint-run/Main.hs
@@ -7,6 +7,7 @@ module Main
   ( main
   ) where
 
+import Data.List
 import System.Environment
 
 main :: IO ()
@@ -16,18 +17,21 @@ main = do
 
 makeMainFile :: [String] -> String
 makeMainFile args
-  = ( showString ""
-      . showString "-- |\n"
-      . showString "-- Copyright: (c) 2018-present Junyoung Clare Jang\n"
-      . showString "-- License: BSD 3-Clause\n"
-      . showString "--\n"
-      . showString "-- An executable to run HLint\n"
-      . showString "module Main\n"
-      . showString "  ( main\n"
-      . showString "  ) where\n"
-      . showString "\n"
-      . showString "import Language.Haskell.HLint.Minicute ( hlint )\n"
-      . showString "\n"
-      . showString "main :: IO ()\n"
-      . showString "main = hlint " . shows args
-    ) "\n"
+  = intercalate "\n"
+    [ "-- |"
+    , "-- Copyright: (c) 2018-present Junyoung Clare Jang"
+    , "-- License: BSD 3-Clause"
+    , "--"
+    , "-- An executable to run HLint"
+    , "module Main"
+    , "  ( main"
+    , "  ) where"
+    , ""
+    , "import System.Environment"
+    , "import Language.Haskell.HLint.Minicute ( hlint )"
+    , ""
+    , "main :: IO ()"
+    , "main = do"
+    , "  extraArgs <- getArgs"
+    , "  hlint $ " <> show args <> " <> extraArgs"
+    ]
