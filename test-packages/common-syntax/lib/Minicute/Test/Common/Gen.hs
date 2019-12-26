b09ac755b0d80868f88eb9a2810f4eafa9123e60
Junyoung Clare Jang <jjc9310@gmail.com>
2019-12-26 00:04:01 -0500

    Add more generators for common syntax

--- test-packages/common-syntax/lib/Minicute/Test/Common/Gen.hs
+++ test-packages/common-syntax/lib/Minicute/Test/Common/Gen.hs
@@ -3,12 +3,15 @@
 -- License: BSD 3-Clause
 module Minicute.Test.Common.Gen
   ( integer
+  , identifierString
   , identifier
+  , primitive
   ) where
 
 import Prelude hiding ( fail )
 
 import Hedgehog
+import Minicute.Data.Common
 
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
@@ -17,6 +20,17 @@ integer :: (MonadGen m) => m Integer
 integer = Gen.integral (Range.constant 0 1000000000000)
 {-# INLINABLE integer #-}
 
-identifier :: (MonadGen m) => m String
-identifier = (:) <$> Gen.choice [Gen.alpha, Gen.constant '_'] <*> Gen.string (Range.linear 0 100) (Gen.choice [Gen.alphaNum, Gen.constant '_'])
+identifierString :: (MonadGen m) => m String
+identifierString
+  = (:)
+    <$> Gen.choice [Gen.alpha, Gen.constant '_']
+    <*> Gen.string (Range.linear 0 100) (Gen.choice [Gen.alphaNum, Gen.constant '_'])
+{-# INLINABLE identifierString #-}
+
+identifier :: (MonadGen m) => m Identifier
+identifier = Identifier <$> identifierString
 {-# INLINABLE identifier #-}
+
+primitive :: (MonadGen m) => m Primitive
+primitive = Gen.element (fmap fst primitivePrecedenceTable)
+{-# INLINABLE primitive #-}
